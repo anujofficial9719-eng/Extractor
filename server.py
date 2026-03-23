983d6ab338fc5f69dc3c6ee186cc5b72db334b2a
anuj9719168804-cyber <anuj9719168804@gmail.com>
2026-03-23 14:52:02 +0530

    Update server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot is running!")
+
+async def web_server():
+    app = web.Application()
+    app.router.add_get("/", handle)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 5000)
+    await site.start()
